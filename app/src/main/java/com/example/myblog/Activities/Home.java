48a680679bf3e1cd30471dae5b1ba1b06b8327f0
Mevlüt KIDIL <mevlutkidilforbusiness@gmail.com>
2023-06-16 18:42:30 +0300

    adding post activity

--- app/src/main/java/com/example/myblog/Activities/Home.java
+++ app/src/main/java/com/example/myblog/Activities/Home.java
@@ -1,8 +1,12 @@
 package com.example.myblog.Activities;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
@@ -10,9 +14,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -22,13 +31,26 @@ import androidx.navigation.ui.NavigationUI;
 import com.bumptech.glide.Glide;
 import com.example.myblog.R;
 import com.example.myblog.databinding.ActivityHome2Binding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Home extends AppCompatActivity {
 
+    private void showMessage(String message) {
+        Toast.makeText(Home.this,message,Toast.LENGTH_LONG).show();
+    }
+
+    private static final int PReqCode= 2;
+    private static final int REQUESCODE = 2;
     private AppBarConfiguration mAppBarConfiguration;
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
@@ -37,6 +59,7 @@ public class Home extends AppCompatActivity {
     TextView popupTitle,popupDescription;
     ProgressBar popupClickProgress;
 private ActivityHome2Binding binding;
+    private Uri pickedImgUri= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +74,7 @@ private ActivityHome2Binding binding;
         currentUser = mAuth.getCurrentUser();
         //ini popup
         iniPopup();
+        setupPopupImageClick();
 
 
         FloatingActionButton   fab =(FloatingActionButton) findViewById(R.id.fab);
@@ -77,6 +101,87 @@ private ActivityHome2Binding binding;
         updateNavHeader();
 
     }
+
+    private void setupPopupImageClick() {
+
+
+        popupPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAndRequestForPermission();
+
+            }
+        });
+
+
+
+
+    }
+
+    private void checkAndRequestForPermission() {
+        if(ContextCompat.checkSelfPermission(Home.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(Home.this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(Home.this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+            }
+
+            else{
+                ActivityCompat.requestPermissions(Home.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+        }
+        else
+            openGallery();
+    }
+
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == REQUESCODE && data!=null){
+            pickedImgUri = data.getData();
+            popupPostImage.setImageURI(pickedImgUri);
+        }
+    }
+
+    private void addPost(Post post) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Posts").push();
+
+        // get post unique ID and update post key
+        String key = myRef.getKey();
+        post.setPostKey(key);
+
+
+        // add post data to firebase database
+
+        myRef.setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                showMessage("Post Added successfully");
+                popupClickProgress.setVisibility(View.INVISIBLE);
+                popupAddBtn.setVisibility(View.VISIBLE);
+                popAddPost.dismiss();
+            }
+
+
+
+
+
+        }
+
+
+        );}
+
+
+
 
     private void iniPopup() {
         popAddPost=new Dialog(this);
@@ -105,20 +210,53 @@ private ActivityHome2Binding binding;
             @Override
             public void onClick(View view) {
                 popupClickProgress.setVisibility(View.VISIBLE);
+
+                if (!popupTitle.getText().toString().isEmpty()&&!popupDescription.getText().toString().isEmpty()&& pickedImgUri != null)
+                {
+
+                    StorageReference storageReference   = FirebaseStorage.getInstance().getReference().child("blog_images");
+                    StorageReference imageFilePath = storageReference.child(pickedImgUri.getLastPathSegment());
+                    imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String imageDownloadLink = uri.toString();
+                                    //creating Post
+                                    Post post = new Post(popupTitle.getText().toString(),popupDescription.getText().toString(),imageDownloadLink,currentUser.getUid(),currentUser.getPhotoUrl().toString());
+                                    // add database
+                                    addPost(post);
+
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    showMessage(e.getMessage());
+                                    popupClickProgress.setVisibility(View.INVISIBLE);
+                                    popupAddBtn.setVisibility(View.VISIBLE);
+
+                                }
+                            });
+                        }
+                    });
+
+
+                }
+                else
+                {
+                    showMessage("Please check your inputs and image..");
+                    popupClickProgress.setVisibility(View.INVISIBLE);
+                }
+
+
             }
         });
 
 
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
@@ -142,5 +280,13 @@ private ActivityHome2Binding binding;
 
 
     }
+
+
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.home, menu);
+            return true;
+        }
 
 }
